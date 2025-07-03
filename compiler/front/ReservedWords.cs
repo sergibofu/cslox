a566a91afa84b0c6031f1430ab07910215b6715a
Sergi <sergibofu@gmail.com>
2025-07-03 16:03:12 +0200

    Scanner:    - added ReservedWords static class]       - Dictionary<string, TokenType> dict stores reserved words       - function returnReservedWordOrNull returns reserved word if exists or null

---
+++ compiler/front/ReservedWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler.front
+{
+    public class ReservedWords
+    {
+        public static Dictionary<string, TokenType> dict = new Dictionary<string, TokenType>()
+        {
+            { "if", TokenType.IF },
+            { "and", TokenType.AND },
+            { "class", TokenType.CLASS },
+            { "else", TokenType.ELSE },
+            { "false", TokenType.FALSE },
+            { "for", TokenType.FOR },
+            { "fun", TokenType.FUN },
+            { "nil", TokenType.NIL },
+            { "or", TokenType.OR },
+            { "print", TokenType.PRINT },
+            { "return", TokenType.RETURN },
+            { "super", TokenType.SUPER },
+            { "this", TokenType.THIS },
+            { "true", TokenType.TRUE },
+            { "var", TokenType.VAR },
+            { "while", TokenType.WHILE },
+        };
+
+        public static TokenType? getTypeOrNull(string type)
+        {
+            if(dict.ContainsKey("type")) return dict[type]; 
+            return null;
+        }
+
+    }
+}
